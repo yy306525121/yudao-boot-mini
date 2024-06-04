15a2dc78b8b16edd18c34120b5e742c71a1c615b
yangzy <306525121@qq.com>
2024-06-04 16:51:27 +0800

    feat: 放假规则

--- yudao-module-school/yudao-module-school-biz/src/main/java/cn/iocoder/yudao/module/school/controller/admin/rule/vo/HolidayRuleSaveReqVO.java
+++ yudao-module-school/yudao-module-school-biz/src/main/java/cn/iocoder/yudao/module/school/controller/admin/rule/vo/HolidayRuleSaveReqVO.java
@@ -1,6 +1,8 @@
 package cn.iocoder.yudao.module.school.controller.admin.rule.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -14,18 +16,23 @@ public class HolidayRuleSaveReqVO {
     private Long id;
 
     @Schema(description = "班级ID的数组json", example = "25025")
+    @NotEmpty(message = "班级不能为空")
     private Set<Long> gradeIds;
 
     @Schema(description = "开始日期")
+    @NotNull(message = "开始日期不能为空")
     private LocalDate startDate;
 
     @Schema(description = "开始节次", example = "29761")
+    @NotNull(message = "开始节次不能为空")
     private Long startTimeSlotId;
 
     @Schema(description = "结束日期")
+    @NotNull(message = "结束日期不能为空")
     private LocalDate endDate;
 
     @Schema(description = "结束节次", example = "15540")
+    @NotNull(message = "结束节次不能为空")
     private Long endTimeSlotId;
 
 }
