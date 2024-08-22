c3178eed19efdd25392dafaf3a2ac9fd2ac0b74e
yangzy <306525121@qq.com>
2024-08-22 17:03:56 +0800

    update

--- yudao-module-school/yudao-module-school-biz/src/main/java/cn/iocoder/yudao/module/school/timefold/contraint/TimeTableConstraintProvider.java
+++ yudao-module-school/yudao-module-school-biz/src/main/java/cn/iocoder/yudao/module/school/timefold/contraint/TimeTableConstraintProvider.java
@@ -4,11 +4,13 @@ import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore;
 import ai.timefold.solver.core.api.score.stream.Constraint;
 import ai.timefold.solver.core.api.score.stream.ConstraintFactory;
 import ai.timefold.solver.core.api.score.stream.ConstraintProvider;
+import ai.timefold.solver.core.api.score.stream.Joiners;
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.NumberUtil;
-import cn.iocoder.yudao.module.school.dal.dataobject.course.CourseTypeDO;
-import cn.iocoder.yudao.module.school.enums.course.CourseTypeEnum;
+import cn.hutool.core.collection.CollectionUtil;
 import cn.iocoder.yudao.module.school.timefold.domain.Lesson;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static ai.timefold.solver.core.api.score.stream.ConstraintCollectors.count;
 import static ai.timefold.solver.core.api.score.stream.Joiners.equal;
@@ -16,209 +18,156 @@ import static ai.timefold.solver.core.api.score.stream.Joiners.equal;
 public class TimeTableConstraintProvider implements ConstraintProvider {
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
-        return new Constraint[] {
+        return new Constraint[]{
                 gradeConflict(constraintFactory),
                 teacherConflict(constraintFactory),
-                timeSlotNormalTimeSlotConflict(constraintFactory),
-                timeSlotNormalDayConflict(constraintFactory),
-                // timeSlotEveningConflict(constraintFactory),
+                //                timeSlotNormalTimeSlotConflict(constraintFactory),
+                //                timeSlotEveningConflict(constraintFactory),
                 continuousConflict(constraintFactory),
-                ordinaryConflict(constraintFactory),
                 ordinaryAndContinuousConflict(constraintFactory),
                 continuousDayConflict(constraintFactory),
+                ordinaryConflict(constraintFactory)
+                // timeSlotNormalDayConflict(constraintFactory),
+                // timeSlotEveningConflict(constraintFactory),
+                // continuousConflict(constraintFactory),
+                // ordinaryConflict(constraintFactory),
+                // ordinaryAndContinuousConflict(constraintFactory),
+                // continuousDayConflict(constraintFactory),
         };
     }
 
     /**
      * 一个班级同时最多只能有一节课
+     *
      * @param constraintFactory
      * @return
      */
-    private Constraint gradeConflict(ConstraintFactory constraintFactory) {
+    public Constraint gradeConflict(ConstraintFactory constraintFactory) {
         return constraintFactory
                 .forEachUniquePair(Lesson.class,
-                        equal(item -> item.getTimeSlot().getId()),
-                        equal(Lesson::getDayOfWeek),
-                        equal(item -> item.getGrade().getId()))
+                        equal((lesson) -> lesson.getTimeSlot().getId()),
+                        equal(Lesson::getGrade))
                 .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("grade constraint");
     }
 
     /**
      * 一个教师同时只能上一节课
+     *
      * @param constraintFactory
      * @return
      */
-    private Constraint teacherConflict(ConstraintFactory constraintFactory) {
+    public Constraint teacherConflict(ConstraintFactory constraintFactory) {
         return constraintFactory
                 .forEachUniquePair(Lesson.class,
-                        equal(item -> item.getTimeSlot().getId()),
-                        equal(Lesson::getDayOfWeek),
-                        equal(item -> item.getTeacher().getId()))
+                        equal((lesson) -> lesson.getTimeSlot().getId()),
+                        equal(Lesson::getTeacher))
                 .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("teacher conflict");
     }
 
     /**
-     * 正课节次为周1~6的每天的2~10节次
+     * 连堂课必须是同一天的连续节次
+     *
      * @param constraintFactory
      * @return
      */
-    private Constraint timeSlotNormalTimeSlotConflict(ConstraintFactory constraintFactory) {
+    public Constraint continuousConflict(ConstraintFactory constraintFactory) {
+        List<Integer> avoidSort = Arrays.asList(6, 7);
+
+        //        return constraintFactory
+        //                .forEach(Lesson.class)
+        //                .filter(Lesson::isContinuousFlag)
+        //                .join(
+        //                        constraintFactory.forEach(Lesson.class)
+        //                                .filter(Lesson::isContinuousFlag),
+        //                        Joiners.equal(Lesson::getContinuousUuid)
+        //                )
+        //                .filter((lesson1, lesson2) -> lesson1.getTimeslot().getDayOfWeek().getValue() == lesson2.getTimeslot().getDayOfWeek().getValue() &&
+        //                                Math.abs(lesson1.getTimeslot().getSort() - lesson2.getTimeslot().getSort()) == 1 &&
+        //                                CollUtil.isNotEmpty(CollectionUtil.subtract(avoidSort, Arrays.asList(lesson1.getTimeslot().getSort(), lesson2.getTimeslot().getSort())))
+        //                        )
+        //                .reward(HardSoftScore.ONE_HARD)
+        //                .justifyWith((lesson1, lesson2, score) -> new ContinuousJustification(lesson1.getTeacher(), lesson1, lesson2, score))
+        //                .asConstraint("continuous conflict");
+
         return constraintFactory
-                .forEach(Lesson.class)
-                .filter(lesson -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
-                .filter(lesson -> lesson.getTimeSlot().getSort() < 2 || lesson.getTimeSlot().getSort() > 10)
-                .penalize(HardSoftScore.ONE_HARD)
-                .asConstraint("timeSlotNormalConflict");
-    }
-
-    /**
-     * 周日不上正课的限制
-     * @param constraintFactory
-     * @return
-     */
-    private Constraint timeSlotNormalDayConflict(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .filter(lesson -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
-                .filter(lesson -> lesson.getDayOfWeek().getValue() == 7)
-                .penalize(HardSoftScore.ONE_HARD)
-                .asConstraint("timeSlotNormalDayConflict");
-    }
-
-
-
-
-
-    /**
-     * 夜自习节次为每天的11~12节次
-     * @param constraintFactory
-     * @return
-     */
-    private Constraint timeSlotEveningConflict(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .filter(lesson -> CourseTypeEnum.EVENING.getType().equals(lesson.getCourseType().getType()))
-                .filter(lesson -> lesson.getTimeSlot().getSort() < 11)
-                .penalize(HardSoftScore.ONE_HARD)
-                .asConstraint("timeSlot evening conflict");
-    }
-
-    /**
-     * 连堂课偏好设置
-     * @param constraintFactory constraintFactory
-     * @return
-     */
-    private Constraint teacherTimeEfficiency(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .join(Lesson.class, equal((lesson) -> lesson.getTeacher().getId()),
-                        equal((lesson) -> lesson.getDayOfWeek().getValue()))
-                .filter((lesson1, lesson2) -> Math.abs(lesson1.getTimeSlot().getSort() - lesson2.getTimeSlot().getSort()) == 1)
-                .reward(HardSoftScore.ONE_SOFT)
-                .asConstraint("Teacher time efficiency");
-    }
-
-    /**
-     * 如果当天已经安排了连堂课，就尽量不在当天安排普通课时
-     * @param constraintFactory constraintFactory
-     * @return
-     */
-    // private Constraint ordinaryAndContinuousConflict(ConstraintFactory constraintFactory) {
-    //     return constraintFactory
-    //             .forEach(Lesson.class)
-    //             .filter((lesson) -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
-    //             .join(Lesson.class, equal((lesson) -> lesson.getDayOfWeek().getValue()),
-    //                     equal((lesson) -> lesson.getSubject().getId()))
-    //             .filter((lesson1, lesson2) -> lesson1.isContinuousFlag() != lesson2.getContinuousFlag().booleanValue())
-    //             .penalize(HardSoftScore.ONE_SOFT)
-    //             .asConstraint("ordinary continuous conflict");
-    // }
-
-    /**
-     * 相同科目相同年级的连堂课时和普通课时不能安排在同一天
-     * @param constraintFactory
-     * @return
-     */
-    private Constraint ordinaryAndContinuousConflict(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .filter((lesson) -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
-                .join(Lesson.class, equal((lesson) -> lesson.getGrade().getId()),
-                        equal((lesson) -> lesson.getSubject().getId()),
-                        equal((lesson) -> lesson.getDayOfWeek().getValue()))
-                .filter((lesson1, lesson2) -> lesson1.isContinuousFlag() != lesson2.isContinuousFlag())
-                .penalize(HardSoftScore.ONE_SOFT)
-                .asConstraint("ordinary continuous conflict");
-    }
-
-    /**
-     * 正课每天不能超过2节
-     * @param constraintFactory
-     * @return
-     */
-    private Constraint teacherTimeMaxLimit(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .filter(lesson -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
-                .groupBy(Lesson::getGrade, Lesson::getDayOfWeek, Lesson::getTeacher, count())
-                .filter((week, teacher, courseType, count) -> count > 2)
-                .penalize(HardSoftScore.ONE_HARD)
-                .asConstraint("teacher time max limit");
-    }
-
-    /**
-     * 连堂课是指同一天内课程节次相邻的两节课
-     * @param constraintFactory
-     * @return
-     */
-    private Constraint continuousConflict(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                .forEach(Lesson.class)
-                .filter(Lesson::isContinuousFlag)
-                .join(Lesson.class, equal(Lesson::getContinuousUuid))
-                .filter((lesson1, lesson2) ->
-                                Math.abs(lesson1.getTimeSlot().getSort() - lesson2.getTimeSlot().getSort()) == 1 &&
-                                lesson1.getDayOfWeek().getValue() == lesson2.getDayOfWeek().getValue()
+                .forEachUniquePair(Lesson.class, Joiners.equal(Lesson::getContinuousUuid))
+                .filter((lesson1, lesson2) -> lesson1.isContinuousFlag() && lesson2.isContinuousFlag())
+                .filter((lesson1, lesson2) -> Math.abs(lesson1.getTimeSlot().getSort() - lesson2.getTimeSlot().getSort()) != 1 ||
+                        lesson1.getDayOfWeek().getValue() != lesson2.getDayOfWeek().getValue() ||
+                        CollUtil.isEmpty(CollectionUtil.subtract(avoidSort, Arrays.asList(lesson1.getTimeSlot().getSort(), lesson2.getTimeSlot().getSort())))
                 )
-                .reward(HardSoftScore.ONE_HARD)
+                .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("continuous conflict");
     }
 
     /**
-     * 一天内不能安排同一个科目的多次连堂课
+     * 相同科目相同年级的连堂课时和普通课时不能安排在同一天
+     *
      * @param constraintFactory
      * @return
      */
-    private Constraint continuousDayConflict(ConstraintFactory constraintFactory) {
+    public Constraint ordinaryAndContinuousConflict(ConstraintFactory constraintFactory) {
         return constraintFactory
-                .forEach(Lesson.class)
-                .filter(Lesson::isContinuousFlag)
-                .join(Lesson.class, equal((lesson) -> lesson.getSubject().getId()),
-                        equal((lesson) -> lesson.getGrade().getId()))
+                .forEachUniquePair(Lesson.class,
+                        Joiners.equal(Lesson::getSubject),
+                        Joiners.equal(Lesson::getGrade))
                 .filter((lesson1, lesson2) ->
-                                !lesson1.getContinuousUuid().equals(lesson2.getContinuousUuid()) &&
-                                lesson1.getDayOfWeek().getValue() == lesson2.getDayOfWeek().getValue()
+                        lesson1.isContinuousFlag() != lesson2.isContinuousFlag() && lesson1.getDayOfWeek().getValue() == lesson2.getDayOfWeek().getValue()
                 )
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint("ordinary continuous conflict");
+    }
+    //
+    // /**
+    //  * 正课每天不能超过2节
+    //  * @param constraintFactory
+    //  * @return
+    //  */
+    // private Constraint teacherTimeMaxLimit(ConstraintFactory constraintFactory) {
+    //     return constraintFactory
+    //             .forEach(Lesson.class)
+    //             .filter(lesson -> CourseTypeEnum.NORMAL.getType().equals(lesson.getCourseType().getType()))
+    //             .groupBy(Lesson::getGrade, Lesson::getDayOfWeek, Lesson::getTeacher, count())
+    //             .filter((week, teacher, courseType, count) -> count > 2)
+    //             .penalize(HardSoftScore.ONE_HARD)
+    //             .asConstraint("teacher time max limit");
+    // }
+    //
+
+
+    /**
+     * 一天内不能安排同一个科目的多次连堂课
+     *
+     * @param constraintFactory
+     * @return
+     */
+    public Constraint continuousDayConflict(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEachUniquePair(Lesson.class, Joiners.equal(Lesson::getSubject),
+                        Joiners.equal(Lesson::getGrade))
+                .filter((lesson1, lesson2) -> lesson1.isContinuousFlag() && lesson2.isContinuousFlag() && !lesson1.getContinuousUuid().equals(lesson2.getContinuousUuid()) && lesson1.getDayOfWeek().getValue() == lesson2.getDayOfWeek().getValue())
                 .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("continuous day conflict");
     }
 
     /**
-     * 一天内不允许出现多节相同科目的普通课时
+     * 同一个班级一天内不允许出现多节相同科目的普通课时
+     *
      * @param constraintFactory
      * @return
      */
-    private Constraint ordinaryConflict(ConstraintFactory constraintFactory) {
+    public Constraint ordinaryConflict(ConstraintFactory constraintFactory) {
         return constraintFactory
                 .forEach(Lesson.class)
                 .filter(lesson -> !lesson.isContinuousFlag())
-                .groupBy(Lesson::getGrade, Lesson::getDayOfWeek, Lesson::getTeacher, count())
-                .filter((week, teacher, courseType, count) -> count > 1)
-                .penalize(HardSoftScore.ONE_SOFT)
+                .groupBy(Lesson::getGrade, Lesson::getDayOfWeek, Lesson::getSubject, count())
+                .filter((grade, week, subject, count) -> count > 1)
+                .penalize(HardSoftScore.ONE_HARD)
                 .asConstraint("ordinary conflict");
     }
+
 
     /**
      * 老师喜欢上课的时间
